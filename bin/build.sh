cea06d5bae074ce1479dc5691dc235b3513ce8d8
Aldebaran Perseke <aldeba_bla@yahoo.com>
2020-10-20 21:49:27 -0300

    Add a CMake file to build the compiler

---
+++ bin/build.sh
@@ -0,0 +1,17 @@
+#!  /usr/bin/ksh
+
+export  CURRENT_DIRECTORY="$( pwd )"
+export  BASE_DIR=$( dirname "${CURRENT_DIRECTORY}/$0" )
+export  BASE_DIR=$( dirname "${BASE_DIR}" )
+export  SOURCE_DIRECTORY="${BASE_DIR}/src/c/main"
+
+cd "${SOURCE_DIRECTORY}"
+if [ -d build ]
+then
+    rm -rf build
+fi
+mkdir build
+cd build
+cmake ../
+cmake --build .
+cd "${CURRENT_DIRECTORY}"
