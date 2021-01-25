8f9f5ace2fb0df34f303f343c8c812a48f22e6d9
Aldebaran Perseke <aldeba_bla@yahoo.com>
2021-01-25 00:39:03 -0300

    Fix broken compileSourceFile tests

--- src/c/test/test_compileSourceFile.c
+++ src/c/test/test_compileSourceFile.c
@@ -44,19 +44,66 @@ int __wrap_regcomp(regex_t *_regex, const char *_pattern, int _cflags)
 
     int result = (int)mock();
 
+    /*
     if (0 == result)
     {
         result = __real_regcomp(_regex, _pattern, _cflags);
+    }
+*/
+    return result;
+}
+
+/*
+    mock for regexec() function
+*/
+
+int __wrap_regexec(regex_t *__restrict__ _regex, char *__restrict__ _string, size_t _nmatch, regmatch_t *__restrict__ _match, int _flags)
+{
+    check_expected(_regex);
+    check_expected(_string);
+    check_expected(_nmatch);
+    check_expected(_match);
+    check_expected(_flags);
+
+    int result = (int)mock();
+    /*
+    if (0 == result)
+    {
+        result = __real_regcomp(_regex, _pattern, _cflags);
+    }
+*/
+    if (0 == strcmp(_string, "./sourceFile"))
+    {
+        _match[1].rm_so = 0;
+        _match[1].rm_eo = 2;
     }
 
     return result;
 }
 
 /*
+    mock for regfree() function
+*/
+
+void __wrap_regfree(regex_t *_regex)
+{
+    check_expected(_regex);
+}
+
+/*
+    wrap initializePreProcessor function
+*/
+
+int __wrap_initializePreProcessor()
+{
+    return (int)mock();
+}
+
+/*
     wrap preprocessor function
 */
 
-void __wrap_preProcessor(char *_sourceDirectory, FILE *_fileInput, FILE *_fileOutput)
+int __wrap_preProcessor(char *_sourceDirectory, FILE *_fileInput, FILE *_fileOutput)
 {
     check_expected(_sourceDirectory);
     check_expected_ptr(_fileInput);
@@ -66,6 +113,16 @@ void __wrap_preProcessor(char *_sourceDirectory, FILE *_fileInput, FILE *_fileOu
     struct stat preProcessorFileStatus;
 
     assert_int_equal(stat(preProcessorFileName, &preProcessorFileStatus), 0);
+
+    return (int)mock();
+}
+
+/*
+    wrap destroyPreProcessor function
+*/
+
+void __wrap_destroyPreProcessor()
+{
 }
 
 /*
@@ -78,14 +135,14 @@ void __wrap_lexicalParser(FILE *_fileInput)
 }
 
 /*
-    test case 001 - fail in the regex
+    test case 001 - fail in the regcomp
 */
 
-static void testCase_failInRegex()
+static void testCase_failInRegcomp()
 {
     char *sourceFileName = "sourceFile.c";
 
-    /*  expected parameters for the regex */
+    /*  expected parameters for the regcomp */
     expect_any(__wrap_regcomp, _regex);
     expect_string(__wrap_regcomp, _pattern, "^(.*/)[^/]+$");
     expect_value(__wrap_regcomp, _cflags, REG_EXTENDED);
@@ -111,7 +168,7 @@ static void testCase_sourceFileWithoutDirectory()
 }
 
 /*
-    test case 001 - source file name with .c extention
+    test case 004 - source file name with .c extention
 */
 
 static void testCase_sourceFileNameDotC()
@@ -125,16 +182,30 @@ static void testCase_sourceFileNameDotC()
     fprintf(sourceFile, "/* test file with just a comment */\n");
     fclose(sourceFile);
 
-    /*  expected parameters for the regex */
+    /*  expected parameters for the regcomp */
     expect_any(__wrap_regcomp, _regex);
     expect_string(__wrap_regcomp, _pattern, "^(.*/)[^/]+$");
     expect_value(__wrap_regcomp, _cflags, REG_EXTENDED);
     will_return(__wrap_regcomp, 0);
 
-    /*  set the expected values for the wrap preProcessor and wrap lexicalParser() functions */
+    /*  expected parameters for the regexec */
+    expect_any(__wrap_regexec, _regex);
+    expect_string(__wrap_regexec, _string, sourceFileName);
+    expect_value(__wrap_regexec, _nmatch, 2);
+    expect_any(__wrap_regexec, _match);
+    expect_value(__wrap_regexec, _flags, 0);
+    will_return(__wrap_regexec, -1);
+
+    /*  expected parameters for the regfree */
+    expect_any(__wrap_regfree, _regex);
+
+    /*  set the expected values for the wrap initializePreProcessor, preProcessor and wrap lexicalParser() functions */
+    will_return(__wrap_initializePreProcessor, 0);
+
     expect_string(__wrap_preProcessor, _sourceDirectory, "./");
     expect_any(__wrap_preProcessor, _fileInput);
     expect_any(__wrap_preProcessor, _fileOutput);
+    will_return(__wrap_preProcessor, 0);
 
     expect_any(__wrap_lexicalParser, _fileInput);
 
@@ -150,7 +221,7 @@ static void testCase_sourceFileNameDotC()
 }
 
 /*
-    test case 002 - source file name without .c extention
+    test case 005 - source file name without .c extention
 */
 
 static void testCase_sourceFileNameWithoutExtention()
@@ -164,16 +235,30 @@ static void testCase_sourceFileNameWithoutExtention()
     fprintf(sourceFile, "/* test file with just a comment */\n");
     fclose(sourceFile);
 
-    /*  expected parameters for the regex */
+    /*  expected parameters for the regcomp */
     expect_any(__wrap_regcomp, _regex);
     expect_string(__wrap_regcomp, _pattern, "^(.*/)[^/]+$");
     expect_value(__wrap_regcomp, _cflags, REG_EXTENDED);
     will_return(__wrap_regcomp, 0);
 
-    /*  set the expected values for the wrap preProcessor and wrap lexicalParser() functions */
+    /*  expected parameters for the regexec */
+    expect_any(__wrap_regexec, _regex);
+    expect_string(__wrap_regexec, _string, sourceFileName);
+    expect_value(__wrap_regexec, _nmatch, 2);
+    expect_any(__wrap_regexec, _match);
+    expect_value(__wrap_regexec, _flags, 0);
+    will_return(__wrap_regexec, 0);
+
+    /*  expected parameters for the regfree */
+    expect_any(__wrap_regfree, _regex);
+
+    /*  set the expected values for the wrap initializePreProcessor, preProcessor and wrap lexicalParser() functions */
+    will_return(__wrap_initializePreProcessor, 0);
+
     expect_string(__wrap_preProcessor, _sourceDirectory, "./");
     expect_any(__wrap_preProcessor, _fileInput);
     expect_any(__wrap_preProcessor, _fileOutput);
+    will_return(__wrap_preProcessor, 0);
 
     expect_any(__wrap_lexicalParser, _fileInput);
 
@@ -195,12 +280,12 @@ static void testCase_sourceFileNameWithoutExtention()
 int runCompileSourceFileTests()
 {
     const struct CMUnitTest testCases[] = {
-        {"test case 001 - fail in the regex", testCase_failInRegex, NULL, NULL},
+        {"test case 001 - fail in the regcomp", testCase_failInRegcomp, NULL, NULL},
         {"test case 002 - source file with a directory", testCase_sourceFileWithDirectory, NULL, NULL},
         {"test case 003 - source file without a directory", testCase_sourceFileWithDirectory, NULL, NULL},
 
-        {"test case 003 - source file name with .c extention", testCase_sourceFileNameDotC, NULL, NULL},
-        {"test case 004 - source file name without .c extention", testCase_sourceFileNameWithoutExtention, NULL, NULL},
+        {"test case 004 - source file name with .c extention", testCase_sourceFileNameDotC, NULL, NULL},
+        {"test case 005 - source file name without .c extention", testCase_sourceFileNameWithoutExtention, NULL, NULL},
     };
 
     return cmocka_run_group_tests_name("compileSourceFile.c tests", testCases, NULL, NULL);
