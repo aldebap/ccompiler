9af320fdf266b95ac4a4f24d2f7e00ddb7571869
Aldebaran Perseke <aldeba_bla@yahoo.com>
2021-01-26 00:00:01 -0300

    Add more test scenarios for compileSourceFile

--- src/c/test/test_compileSourceFile.c
+++ src/c/test/test_compileSourceFile.c
@@ -19,18 +19,10 @@
 #include "options.h"
 
 /*
-    globals
-*/
-
-char preProcessorFileName[MAXPATHLEN];
-
-/*
     prototypes
 */
 
 int compileSourceFile(char *_fileName);
-
-int __real_regcomp(regex_t *_regex, const char *_pattern, int _cflags);
 
 /*
     mock for regcomp() function
@@ -42,15 +34,7 @@ int __wrap_regcomp(regex_t *_regex, const char *_pattern, int _cflags)
     check_expected(_pattern);
     check_expected(_cflags);
 
-    int result = (int)mock();
-
-    /*
-    if (0 == result)
-    {
-        result = __real_regcomp(_regex, _pattern, _cflags);
-    }
-*/
-    return result;
+    return (int)mock();
 }
 
 /*
@@ -65,20 +49,13 @@ int __wrap_regexec(regex_t *__restrict__ _regex, char *__restrict__ _string, siz
     check_expected(_match);
     check_expected(_flags);
 
-    int result = (int)mock();
-    /*
-    if (0 == result)
-    {
-        result = __real_regcomp(_regex, _pattern, _cflags);
-    }
-*/
     if (0 == strcmp(_string, "./sourceFile"))
     {
         _match[1].rm_so = 0;
         _match[1].rm_eo = 2;
     }
 
-    return result;
+    return (int)mock();
 }
 
 /*
@@ -108,11 +85,6 @@ int __wrap_preProcessor(char *_sourceDirectory, FILE *_fileInput, FILE *_fileOut
     check_expected(_sourceDirectory);
     check_expected_ptr(_fileInput);
     check_expected_ptr(_fileOutput);
-
-    /*  make sure preprocessor file exist */
-    struct stat preProcessorFileStatus;
-
-    assert_int_equal(stat(preProcessorFileName, &preProcessorFileStatus), 0);
 
     return (int)mock();
 }
@@ -157,6 +129,52 @@ static void testCase_failInRegcomp()
 
 static void testCase_sourceFileWithDirectory()
 {
+    char *sourceFileName = "./sourceFile";
+
+    /*  generate a source file */
+    FILE *sourceFile;
+
+    sourceFile = fopen(sourceFileName, "w");
+    fprintf(sourceFile, "/* test file with just a comment */\n");
+    fclose(sourceFile);
+
+    /*  expected parameters for the regcomp */
+    expect_any(__wrap_regcomp, _regex);
+    expect_string(__wrap_regcomp, _pattern, "^(.*/)[^/]+$");
+    expect_value(__wrap_regcomp, _cflags, REG_EXTENDED);
+    will_return(__wrap_regcomp, 0);
+
+    /*  expected parameters for the regexec */
+    expect_any(__wrap_regexec, _regex);
+    expect_string(__wrap_regexec, _string, sourceFileName);
+    expect_value(__wrap_regexec, _nmatch, 2);
+    expect_any(__wrap_regexec, _match);
+    expect_value(__wrap_regexec, _flags, 0);
+    will_return(__wrap_regexec, 0);
+
+    /*  expected parameters for the regfree */
+    expect_any(__wrap_regfree, _regex);
+
+    /*  set the expected values for the wrap initializePreProcessor, preProcessor and wrap lexicalParser() functions */
+    will_return(__wrap_initializePreProcessor, 0);
+
+    expect_string(__wrap_preProcessor, _sourceDirectory, "./");
+    expect_any(__wrap_preProcessor, _fileInput);
+    expect_any(__wrap_preProcessor, _fileOutput);
+    will_return(__wrap_preProcessor, 0);
+
+    expect_any(__wrap_lexicalParser, _fileInput);
+
+    /*  make sure preprocessor file doesn't exist prior to call compileSourceFile */
+    char preProcessorFileName[MAXPATHLEN];
+    struct stat preProcessorFileStatus;
+
+    strcpy(preProcessorFileName, "sourceFile.i");
+    assert_int_not_equal(stat(preProcessorFileName, &preProcessorFileStatus), 0);
+
+    assert_int_equal(compileSourceFile(sourceFileName), 0);
+
+    remove(sourceFileName);
 }
 
 /*
@@ -165,6 +183,52 @@ static void testCase_sourceFileWithDirectory()
 
 static void testCase_sourceFileWithoutDirectory()
 {
+    char *sourceFileName = "sourceFile";
+
+    /*  generate a source file */
+    FILE *sourceFile;
+
+    sourceFile = fopen(sourceFileName, "w");
+    fprintf(sourceFile, "/* test file with just a comment */\n");
+    fclose(sourceFile);
+
+    /*  expected parameters for the regcomp */
+    expect_any(__wrap_regcomp, _regex);
+    expect_string(__wrap_regcomp, _pattern, "^(.*/)[^/]+$");
+    expect_value(__wrap_regcomp, _cflags, REG_EXTENDED);
+    will_return(__wrap_regcomp, 0);
+
+    /*  expected parameters for the regexec */
+    expect_any(__wrap_regexec, _regex);
+    expect_string(__wrap_regexec, _string, sourceFileName);
+    expect_value(__wrap_regexec, _nmatch, 2);
+    expect_any(__wrap_regexec, _match);
+    expect_value(__wrap_regexec, _flags, 0);
+    will_return(__wrap_regexec, -1);
+
+    /*  expected parameters for the regfree */
+    expect_any(__wrap_regfree, _regex);
+
+    /*  set the expected values for the wrap initializePreProcessor, preProcessor and wrap lexicalParser() functions */
+    will_return(__wrap_initializePreProcessor, 0);
+
+    expect_string(__wrap_preProcessor, _sourceDirectory, "./");
+    expect_any(__wrap_preProcessor, _fileInput);
+    expect_any(__wrap_preProcessor, _fileOutput);
+    will_return(__wrap_preProcessor, 0);
+
+    expect_any(__wrap_lexicalParser, _fileInput);
+
+    /*  make sure preprocessor file doesn't exist prior to call compileSourceFile */
+    char preProcessorFileName[MAXPATHLEN];
+    struct stat preProcessorFileStatus;
+
+    strcpy(preProcessorFileName, "sourceFile.i");
+    assert_int_not_equal(stat(preProcessorFileName, &preProcessorFileStatus), 0);
+
+    assert_int_equal(compileSourceFile(sourceFileName), 0);
+
+    remove(sourceFileName);
 }
 
 /*
@@ -210,6 +274,7 @@ static void testCase_sourceFileNameDotC()
     expect_any(__wrap_lexicalParser, _fileInput);
 
     /*  make sure preprocessor file doesn't exist prior to call compileSourceFile */
+    char preProcessorFileName[MAXPATHLEN];
     struct stat preProcessorFileStatus;
 
     strcpy(preProcessorFileName, "sourceFile.i");
@@ -263,6 +328,7 @@ static void testCase_sourceFileNameWithoutExtention()
     expect_any(__wrap_lexicalParser, _fileInput);
 
     /*  make sure preprocessor file doesn't exist prior to call compileSourceFile */
+    char preProcessorFileName[MAXPATHLEN];
     struct stat preProcessorFileStatus;
 
     strcpy(preProcessorFileName, "sourceFile.i");
